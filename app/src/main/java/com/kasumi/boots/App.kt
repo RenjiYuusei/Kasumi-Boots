cb3b0d125f0fb825f87129fad8a439a69ebe0723
RenjiYuusei <daoluc.yy@gmail.com>
2025-10-07 22:06:34 +0700

    fix

--- app/src/main/java/com/kasumi/boots/App.kt
+++ app/src/main/java/com/kasumi/boots/App.kt
@@ -1,20 +1,11 @@
 package com.kasumi.boots
 
 import android.app.Application
-import android.content.res.Configuration
 import com.topjohnwu.superuser.Shell
-import java.util.*
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        
-        // Set Vietnamese as default locale
-        val locale = Locale("vi")
-        Locale.setDefault(locale)
-        val config = Configuration(resources.configuration)
-        config.setLocale(locale)
-        resources.updateConfiguration(config, resources.displayMetrics)
         
         // Configure libsu main shell
         Shell.enableVerboseLogging = BuildConfig.DEBUG
